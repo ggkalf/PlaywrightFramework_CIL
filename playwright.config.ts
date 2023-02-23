b7d1bf08e0855eb87397d8a9253e09c24ca81433
panosyfan <panagiotis.yfantis@camelotls.com>
2023-02-23 11:25:58 +0200

    reconfigure playwright.config.ts according to playwright template

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,32 +12,31 @@ import { devices } from '@playwright/test';
  */
 
 const config: PlaywrightTestConfig = {
-  // testDir: './tests',
-  // /* Maximum time one test can run for. */
-  // timeout: 30 * 1000,
-  // expect: {
-  //   /**
-  //    * Maximum time expect() should wait for the condition to be met.
-  //    * For example in `await expect(locator).toHaveText();`
-  //    */
-  //   timeout: 5000,
-  // },
-  // /* Run tests in files in parallel */
-  // fullyParallel: true,
-  // /* Fail the build on CI if you accidentally left test.only in the source code. */
-  // forbidOnly: !!process.env.CI,
-  // /* Retry on CI only */
-  // retries: process.env.CI ? 1 : 0,
-  // /* Opt out of parallel tests on CI. */
+  // testDir: '.src/specs/REL42/FpgIframeSpec/',
+  /* Maximum time one test can run for. */
+  timeout: 30 * 1000,
+  expect: {
+    /**
+     * Maximum time expect() should wait for the condition to be met.
+     * For example in `await expect(locator).toHaveText();`
+     */
+    timeout: 5000,
+  },
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 1 : 0,
+  /* Opt out of parallel tests on CI. */
 
-  // // Max number of worker processes, max is 10 in MAC
-  // // workers: process.env.CI ? 2 : undefined,
-
+  // Max number of worker processes, max is 10 in MAC
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   // reporter: 'html',
-  // /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  globalSetup: 'src/utils/globalSetup.ts',
   use: {
     headless: true,
     // Take a screenshot of failed tests in /Users/gerasimoskalfountzos/PlaywrightFramework/test-results/
@@ -87,8 +86,6 @@ const config: PlaywrightTestConfig = {
       },
     },
   ],
-  // workers: 1,
-  globalSetup: 'src/utils/globalSetup.ts',
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
   // outputDir: 'test-results/',
